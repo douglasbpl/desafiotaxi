601677bd7cb93a39b961236f9c8155d02d653d7e
karynnemoreira <karynnemoreira@yahoo.com.br>
2021-10-30 22:46:18 -0300

    teste

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -2,46 +2,6 @@ import React from "react"; import * as S from "./styled";
 
 export function Main() {
   return (
-    <S.ContainerMain>
- 
- <h2> Why Ride With Uloax </h2>
-
-
-<S.ContainerBoxOne> 
-  <S.Imag>
-  <img src = "https://w7.pngwing.com/pngs/983/608/png-transparent-wallet-computer-icons-wallet-brown-leather-material.png" alt =" carteira" width="40%"/>
- </S.Imag>
- <S.Texts>
-  <h1> Uloax for Every Pocket </h1>
-  <p> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as It is a long established fact that a reader will be distracted by the readable c</p>
-  <button> BOOK NOW </button>
-  </S.Texts>
-</S.ContainerBoxOne>
-
-
-<S.ContainerBoxTwo> 
-
- <S.Texts>
-  <h1> Secure and Safer Rides </h1>
-  <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as It is a long established fact that a reader will be distracted by the readable c</p>
-  <button> BOOK NOW </button>
-  </S.Texts>
-
- <S.Imag>
-  <img src = "https://img2.gratispng.com/20180601/xtk/kisspng-taxi-car-rental-renting-rishikesh-travel-5b10d3a6c67bc6.046174311527829414813.jpg" alt =" taxi" width="40%"/>
- </S.Imag>
-
-</S.ContainerBoxTwo>
-
-
-
-
-
-
-
-
-
-
-    </S.ContainerMain>
+   <div></div>
     );
    }   
